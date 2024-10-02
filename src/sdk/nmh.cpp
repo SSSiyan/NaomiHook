32b21bef3750eeaa5635bf008ad86ad76cb31508
SSSiyan <gsssiyan@gmail.com>
2024-10-02 20:37:28 +0100

    PlayerTracker won't crash before game init

--- src/sdk/nmh.cpp
+++ src/sdk/nmh.cpp
@@ -1,18 +1,17 @@
 #include "nmh.hpp"
 
 namespace nmh_sdk {
+	mHRBattle* get_mediator() {
+		static uintptr_t gpBattlePtr = g_framework->get_module().as<uintptr_t>() + 0x843584;
+		mHRBattle* gpBattle = (mHRBattle*)*(uintptr_t*)gpBattlePtr;
+		if (gpBattle)
+			return gpBattle;
+		 return NULL;
+	}
 	mHRPc* get_local_player() {
-		uintptr_t gpBattlePtr = g_framework->get_module().as<uintptr_t>() + 0x843584;
-		static mHRBattle* gpBattle = (mHRBattle*)*(uintptr_t*)gpBattlePtr;
-		if (gpBattle->mpPc)
+		static mHRBattle* gpBattle = get_mediator();
+		if (gpBattle && gpBattle->mpPc)
 			return gpBattle->mpPc;
 		return NULL;
-	}
-	mHRBattle* get_mediator() {
-		uintptr_t gpBattlePtr = g_framework->get_module().as<uintptr_t>() + 0x843584;
-		static mHRBattle* gpBattle = (mHRBattle*)*(uintptr_t*)gpBattlePtr;
-		if (gpBattle)
-			return (mHRBattle*)*(uintptr_t*)gpBattlePtr;
-		 return NULL;
 	}
 }
